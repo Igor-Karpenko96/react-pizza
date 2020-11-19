138c7ab94f88b14a4809cda3d7589ca47695c723
Igor Karpenko <karpenkoigor961@gmail.com>
2020-11-19 22:58:35 +0200

    Categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Categories({ items, onClickItem }) {
+    const [activeItem, setActiveItem] = React.useState(null);
+    
+    const onSelectItem = (index) => {
+        setActiveItem(index);
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                <li 
+                    onClick={() => onSelectItem(null)}
+                    className={activeItem === null ? 'active' : ''} 
+                    >
+                        Все
+                </li>
+                {items && items.map((name, index) => 
+                    <li 
+                        className={activeItem === index ? 'active' : ''} 
+                        onClick={() => onSelectItem(index)} 
+                        key={`${name}_${index}`}>{name}
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
